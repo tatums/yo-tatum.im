d9e99e939701f15a757650d49958f33af4ef42cf
tatums <tatum@ashlandstudios.com>
2014-10-18 20:04:04 -0500

    fixed the lines issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -413,8 +413,7 @@ module.exports = function (grunt) {
     'cssmin',
     'uglify',
     'filerev',
-    'usemin',
-    'htmlmin'
+    'usemin'
   ]);
 
   grunt.registerTask('default', [
--- app/scripts/directives/ng-prism.js
+++ app/scripts/directives/ng-prism.js
@@ -9,19 +9,15 @@
 *
 * the files prismjs and prism css must be included in HTML, use class "language-XXX" to specify language
 * */
-angular.module('yoTatumimApp').directive('ngPrism', ['$interpolate', function ($interpolate) {
-       "use strict";
+angular.module('yoTatumimApp').directive('ngPrism', ['$interpolate', function () {
+       'use strict';
        return {
            restrict: 'E',
            template: '<pre><code ng-transclude></code></pre>',
            replace: true,
            transclude: true,
-           link: function (scope, elm) {
-               //var tmp = $interpolate(elm.find('code').text())(scope);
-
-               Prism.highlightAll();
-               //Prism.highlightElement(elm.find('code').html())
-               //elm.find('code').html(Prism.highlightElement(tmp).value);
+           link: function () {
+              Prism.highlightAll();
            }
        };
    }]);
